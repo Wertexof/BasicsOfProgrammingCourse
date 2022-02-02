347ca4c99dde5edb08f1a4b82742610ec4aea2c4
Wertexof <michael.sazonov2000@gmail.com>
2022-02-02 08:40:52 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/vectors_project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vectors_project.dir/main.c.obj"
+  "libvectors_project.dll.a"
+  "vectors_project.exe"
+  "vectors_project.exe.manifest"
+  "vectors_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/vectors_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/vectors_project.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for vectors_project.
+# This may be replaced when dependencies are built.
