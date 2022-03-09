f3171d3b80a5c2a4281917dc5f6bd4e8d18176da
Wertexof <michael.sazonov2000@gmail.com>
2022-03-09 03:31:42 +0300

    task 16

--- libs/tasks.c
+++ libs/tasks.c
@@ -822,6 +822,33 @@ void task15(char *s, char *res) {
 }
 
 //№16.
+WordBeforeFirstWordWithAReturnCode wordPrecedingTheFirstOccurrence(char *s1, char *s2) {
+    WordDescriptor w1, w2, lastW;
+    char *start = s1;
+
+    if (!getWord(start, &w1) || !getWord(s2, &w2))
+        return EMPTY_STRING;
+
+    while (getWord(start, &w1)) {
+        char *cmp = s2;
+        while (getWord(cmp, &w2)) {
+            if (areWordsEqual(w1, w2)) {
+                if (start == s1)
+                    return FIRST_WORD_WITH_A;
+                else {
+                    return WORD_FOUND;
+                }
+            }
+
+            cmp = w2.end;
+        }
+
+        start = w1.end;
+        lastW = w1;
+    }
+
+    return NOT_FOUND_A_WORD_WITH_A;
+}
 
 //№17.
 
